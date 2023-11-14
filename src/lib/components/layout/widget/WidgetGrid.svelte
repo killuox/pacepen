c483d404b9de894703d3c4bd61fa785fcefd42ac
Antonin Arsenault <aarsenault@korsr.studio>
2023-11-13 21:36:35 -0500

    Continue widget layout setup

--- src/lib/components/layout/widget/WidgetGrid.svelte
+++ src/lib/components/layout/widget/WidgetGrid.svelte
@@ -1,24 +1,44 @@
 <script lang="ts">
 	import Pomodoro from '$lib/components/widgets/pomodoro/Pomodoro.svelte';
-    let test = "quotes";
+    import { currentTemplate } from '$lib/stores/template';
+	import type { SvelteComponent } from 'svelte';
+
+    const templatesComponents = {
+        pomodoro: Pomodoro,
+
+    } as {
+        [key: string]: typeof SvelteComponent;
+    }
+
+    $: formattedTemplateArea = $currentTemplate.area.reduce((acc, curr) => {
+        const rowArea = curr.reduce((acc, curr) => {
+            return acc + `${curr} `;
+        }, '');
+
+        return acc + `"${rowArea}" `;
+    }, '');
+
+    $: templateWidgets = $currentTemplate.area.flat().reduce((acc: string[], curr: string) => { // return only unique widgets
+        if (!acc.includes(curr)) {
+            acc.push(curr);
+        }
+        return acc;
+    }, []);
+    
 </script>
 
-<div class="widget-area h-full">
-    <div style="grid-area: {test}">
-        <Pomodoro />
-    </div>
+<div class="widget-area h-full" style="grid-template-areas: {formattedTemplateArea}">
+    {#each templateWidgets as widget}
+        <div class="{widget}" style="grid-area: {widget}">
+            <svelte:component this={templatesComponents[widget]} />
+        </div>
+    {/each}
 </div>
 
 <style>
     .widget-area {
         display: grid;
         gap: 1rem;
-        grid-template-areas:
-            "pomodoro pomodoro header header header"
-            "pomodoro pomodoro header header header"
-            "tasks tasks tasks notes notes"
-            "weather weather weather news news"
-            "calendar calendar calendar quotes quotes";
     }
 
     .header {
--- src/lib/stores/template.ts
+++ src/lib/stores/template.ts
@@ -1,51 +1,86 @@
 import { writable } from 'svelte/store';
 
 const templateList = [
-    {
-        name: 'Default',
-        isActive: true,
-    },
-    {
-        name: 'Studying',
-        isActive: false,
-    },
-    {
-        name: 'Health',
-        isActive: false,
-    },
-    {
-        name: 'Habit',
-        isActive: false,
-    },
-    {
-        name: 'Mood',
-        isActive: false,
-    },
+	{
+		name: 'Default',
+		isActive: true,
+		area: [
+			['pomodoro', 'pomodoro', 'todo', 'todo', 'todo'],
+			['notes', 'notes', 'todo', 'todo', 'todo'],
+			['notes', 'notes', 'todo', 'todo', 'todo'],
+			['notes', 'notes', 'x', 'x', 'x'],
+			['z', 'z', 'x', 'x', 'x']
+		]
+	},
+	{
+		name: 'Studying',
+		isActive: false,
+        area: [
+			['sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep']
+		]
+	},
+	{
+		name: 'Health',
+		isActive: false,
+        area: [
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep']
+		]
+	},
+	{
+		name: 'Habit',
+		isActive: false,
+        area: [
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep']
+		]
+	},
+	{
+		name: 'Mood',
+		isActive: false,
+        area: [
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep'],
+			['Sleep', 'Sleep', 'Sleep', 'Sleep', 'Sleep']
+		]
+	}
 ];
 
 export const templates = writable(templateList);
 
-type Template = typeof templateList[0];
+type Template = (typeof templateList)[0];
 
 export const currentTemplate = writable(templateList[0]);
 
 export const updateCurrentTemplate = (template: Template) => {
-    currentTemplate.update(() => template);
+	currentTemplate.update(() => template);
 
-    // add isActive to templates
-    templates.update((list) => {
-        return list.map((item) => {
-            if (item.name === template.name) {
-                return {
-                    ...item,
-                    isActive: true,
-                };
-            } else {
-                return {
-                    ...item,
-                    isActive: false,
-                };
-            }
-        });
-    });
-}
+	// add isActive to templates
+	templates.update((list) => {
+		return list.map((item) => {
+			if (item.name === template.name) {
+				return {
+					...item,
+					isActive: true
+				};
+			} else {
+				return {
+					...item,
+					isActive: false
+				};
+			}
+		});
+	});
+};
